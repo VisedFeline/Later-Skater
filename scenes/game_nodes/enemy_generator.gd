674189f841e0c014a7129d24ef7987c92f58b7bf
root <root@DESKTOP-E0B19KH>
2023-12-27 19:15:14 +0200

    made enemey generation times random

--- scenes/game_nodes/enemy_generator.gd
+++ scenes/game_nodes/enemy_generator.gd
@@ -6,13 +6,16 @@ var game_manager = self.get_parent()
 @onready
 var skater = game_manager.get_node("skater")
 
-
 @onready
 var direct_obstacle_timer = self.get_node("direct_obstacle_timer")
 @onready
 var enemy_cooldown_timer = self.get_node("enemy_cooldown_timer")
 @onready
 var TIMERS = [direct_obstacle_timer, enemy_cooldown_timer]
+
+# First element is 'from' float, second is 'to' gloat
+const RANDOM_DIRECT_ENEMY_GENERATION_RANGE = [3, 8]
+const RANDOM_OBSTACLE_GENERATION_RANGE = 2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -29,7 +32,15 @@ func _process(delta):
 func _on_direct_obstacle_timer_timeout():
     var obstacle_y_position = skater.position.y
     game_manager.generate_enemy(obstacle_y_position)
+    var new_wait_time = randf_range(RANDOM_DIRECT_ENEMY_GENERATION_RANGE[0],
+                                    RANDOM_DIRECT_ENEMY_GENERATION_RANGE[1])
+    direct_obstacle_timer.set_wait_time(new_wait_time)
+    print("dwt: " + str(direct_obstacle_timer.get_wait_time()))
 
 
 func _on_enemy_cooldown_timer_timeout():
     game_manager.generate_enemy()
+    var new_wait_time = randf_range(0, RANDOM_OBSTACLE_GENERATION_RANGE)
+    enemy_cooldown_timer.set_wait_time(new_wait_time)
+    print("ewt: " + str(enemy_cooldown_timer.get_wait_time()))
+    
