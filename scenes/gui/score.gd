45eded612eec613bfff5c923e1d7c2ef516911c6
root <root@DESKTOP-E0B19KH>
2024-01-01 02:09:38 +0200

    exported submitted version and modified assets

---
+++ scenes/gui/score.gd
@@ -0,0 +1,13 @@
+extends Label
+
+@onready
+var scorer = get_tree().root.get_node("level").get_node("skater").get_node("scorer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    self.text = "YOUR SCORE: " + str(scorer.score)
